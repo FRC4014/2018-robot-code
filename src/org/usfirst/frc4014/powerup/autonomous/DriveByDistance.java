84c7e9396ed082e1eff68337cdd23a37f5874829
Michael Quinn <mquinn2020@hightechhigh.org>
2018-01-21 16:19:45 -0800

    Add encoder code in drive by distance

--- src/org/usfirst/frc4014/powerup/autonomous/DriveByDistance.java
+++ src/org/usfirst/frc4014/powerup/autonomous/DriveByDistance.java
@@ -6,16 +6,23 @@ import edu.wpi.first.wpilibj.command.Command;
 
 public class DriveByDistance extends Command{
 
+	private static double WHEEL_DIAMETER = 6;
+	private static double GEAR_RATIO = 5.45;
+	private static double PULSES_PER_ROTATION = 4;
+	private static double DISTANCE_PER_PULSE = 
+			Math.PI * WHEEL_DIAMETER / GEAR_RATIO * PULSES_PER_ROTATION;
+
 	private final DriveTrain driveTrain;
+	
 	private double speed;
 	private double distance;
-	
+			
 	public DriveByDistance(DriveTrain driveTrain, double speed, double distance) {
 		this.driveTrain = driveTrain;
 		this.speed = speed;
 		this.distance = distance;
 	}
-	
+	 
 	@Override
 	protected boolean isFinished() {
 		// TODO Auto-generated method stub
