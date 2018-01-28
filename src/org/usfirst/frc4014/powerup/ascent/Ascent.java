6e0d34072e7fd0f4db0ffc6bbe1987abebea54ef
rhopper2021 <rhopper2021@hightechhigh.org>
2018-01-27 16:53:05 -0800

    make claw and ascent subsystems, currently both are empty

---
+++ src/org/usfirst/frc4014/powerup/ascent/Ascent.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc4014.powerup.ascent;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Ascent extends Subsystem {
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/org/usfirst/frc4014/powerup/claw/Claw.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc4014.powerup.claw;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Claw extends Subsystem {
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
