811567751f814265315e676617d8581dc7488b79
lovelace <lovelace@ht3.com>
2018-02-10 09:30:45 -0800

    Use preference for drive distance test.

--- src/org/usfirst/frc4014/powerup/autonomous/TestPosition.java
+++ src/org/usfirst/frc4014/powerup/autonomous/TestPosition.java
@@ -4,10 +4,12 @@ import org.usfirst.frc4014.powerup.drivetrain.DriveTrain;
 
 import com.kauailabs.navx.frc.AHRS;
 
+import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class TestPosition extends CommandGroup{
 	public TestPosition (DriveTrain driveTrain, AHRS ahrs){
-		addSequential (new DriveByDistance(driveTrain, 1, 30));
+		addSequential (new DriveByDistance(driveTrain, 1, 
+				Preferences.getInstance().getDouble("DriveDistanceInches", 12)));
 	}
 }
