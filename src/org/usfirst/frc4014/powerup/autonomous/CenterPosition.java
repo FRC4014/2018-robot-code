2010783099014b5e5c7de881d0a4626b0eefd131
Nicholas <njennings2019@hightechhigh.org>
2018-02-16 10:51:02 -0800

    redo math for center position

--- src/org/usfirst/frc4014/powerup/autonomous/CenterPosition.java
+++ src/org/usfirst/frc4014/powerup/autonomous/CenterPosition.java
@@ -8,17 +8,18 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class CenterPosition extends CommandGroup{
 	public CenterPosition(DriveTrain driveTrain, boolean isAllySwitchOnLeft) {
+		addSequential(new DriveByDistance(driveTrain, 1, 25));
 		if(isAllySwitchOnLeft) {
-			addSequential(new CustomPIDPivotByGyro(21));
-			addSequential(new DriveByDistance(driveTrain, 1, 149.96));
-			addSequential(new CustomPIDPivotByGyro(-21));
-			//TODO add drop code
+			addSequential(new CustomPIDPivotByGyro(67.5));
+			addSequential(new DriveByDistance(driveTrain, 1, 141.16));
+			addSequential(new CustomPIDPivotByGyro(-67.4));
 		} else {
-			addSequential(new CustomPIDPivotByGyro(-21));
-			addSequential(new DriveByDistance(driveTrain, 1, 149.96));
-			addSequential(new CustomPIDPivotByGyro(21));
-			//TODO add drop code
+			addSequential(new CustomPIDPivotByGyro(-65));
+			addSequential(new DriveByDistance(driveTrain, 1, 133.125));
+			addSequential(new CustomPIDPivotByGyro(65));
 		}
+		addSequential(new DriveByDistance(driveTrain, 1, 42));
+		//TODO add drop code
 		//TODO extend fred (if we end up using fred)
 	}
 }
